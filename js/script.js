9fa6a499467885d974a31b430c41e5b52c26819d
Ítalo Martins <italomartinsg@gmail.com>
2026-09-02 20:09:23 -0300

    feat: add revenue chart and delete confirmation

--- js/script.js
+++ js/script.js
@@ -11,35 +11,51 @@ const categoriesByType = {
   receita: ["Salário", "Freelance", "Investimentos"],
   despesa: ["Moradia", "Alimentação", "Lazer", "Transporte"],
 };
+
 const currencyFormatter = new Intl.NumberFormat("pt-BR", {
   style: "currency",
   currency: "BRL",
 });
+
 let totalExpense = 0;
 let totalRevenue = 0;
 let totalBalance = totalRevenue - totalExpense;
+
 const transactionList = document.querySelector(".transacao-lista");
 const valueRevenue = document.querySelector(".valor-receita");
 const valueExpense = document.querySelector(".valor-despesa");
 const valueBalance = document.querySelector(".valor-total");
+
 const filterTypeSelect = document.querySelector("#filtro-tipo-transacao");
 const filterCategorySelect = document.querySelector(
   "#filtro-categoria-transacao",
 );
+
 const filterDate = document.querySelector("#filtro-data");
 const btnClearFilters = document.querySelector(".limpar-filtros");
+
 const allCategories = [
   "todos",
   ...categoriesByType["receita"],
   ...categoriesByType["despesa"],
 ];
+
 const transactions = [];
+
 const financialChart = document.querySelector("#financial-chart");
+
 const expensesCategoryChart = document.querySelector(
   "#expenses-category-chart",
 );
+
+const revenuesCategoryChart = document.querySelector(
+  "#revenues-category-chart",
+);
+
 let chart;
 let categoryChart;
+let revenueCategoryChart;
+
 function idGenerator() {
   const highestID = transactions.reduce((accumulator, currentValue) => {
     if (accumulator < currentValue.id) {
@@ -48,6 +64,7 @@ function idGenerator() {
 
     return accumulator;
   }, 0);
+
   return highestID + 1;
 }
 
@@ -66,6 +83,7 @@ function getCategories(event) {
         ["todos", ...categoriesByType[filterTypeSelect.value]],
         filterCategorySelect,
       );
+
       filterTransactions();
     }
   }
@@ -73,6 +91,7 @@ function getCategories(event) {
 
 function updateCategories(transactionValues, categorySelect) {
   categorySelect.textContent = "";
+
   transactionValues.forEach((type) => {
     const newOption = document.createElement("option");
 
@@ -82,23 +101,29 @@ function updateCategories(transactionValues, categorySelect) {
     categorySelect.appendChild(newOption);
   });
 }
+
 function calculateFinancialSummary() {
   const expense = transactions.filter(
     (transaction) => transaction.type === "despesa",
   );
+
   const revenue = transactions.filter(
     (transaction) => transaction.type === "receita",
   );
+
   totalRevenue = revenue.reduce((accumulator, currentValue) => {
     accumulator += currentValue.value;
     return accumulator;
   }, 0);
+
   totalExpense = expense.reduce((accumulator, currentValue) => {
     accumulator += currentValue.value;
     return accumulator;
   }, 0);
+
   totalBalance = +(totalRevenue - totalExpense).toFixed(2);
 }
+
 function updateFinancialSummary() {
   valueRevenue.textContent = currencyFormatter.format(totalRevenue);
   valueExpense.textContent = currencyFormatter.format(totalExpense);
@@ -107,17 +132,22 @@ function updateFinancialSummary() {
 
 function saveTransactions() {
   const transactionString = JSON.stringify(transactions);
+
   localStorage.setItem("transactions", transactionString);
 }
+
 function loadTransactions() {
   const storagedTransactions = localStorage.getItem("transactions");
+
   if (!storagedTransactions) {
     return;
   } else {
     const storagedObj = JSON.parse(storagedTransactions);
+
     transactions.push(...storagedObj);
   }
 }
+
 function filterTransactions() {
   let filteredTransactions = transactions;
 
@@ -126,6 +156,7 @@ function filterTransactions() {
       (transaction) => transaction.type === filterTypeSelect.value,
     );
   }
+
   if (filterCategorySelect.value !== "todos") {
     filteredTransactions = filteredTransactions.filter(
       (transaction) => transaction.category === filterCategorySelect.value,
@@ -137,55 +168,81 @@ function filterTransactions() {
       (transaction) => transaction.date === filterDate.value,
     );
   }
+
   showHistory(filteredTransactions);
 }
+
 function clearFilters() {
   filterTypeSelect.value = "todos";
   filterDate.value = "";
+
   updateCategories(allCategories, filterCategorySelect);
+
   filterTransactions();
 }
+
 function showHistory(transactionsToShow) {
   transactionList.textContent = "";
+
   transactionsToShow.forEach((transaction) => {
     const newLi = document.createElement("li");
     const newButton = document.createElement("button");
     const cleanDate = transaction.date.split("-");
+
     newLi.dataset.id = transaction.id;
+
     newLi.textContent = `${transaction.description} ${currencyFormatter.format(transaction.value)} ${transaction.category} ${cleanDate[2]}/${cleanDate[1]}/${cleanDate[0]} `;
+
     newButton.textContent = "Excluir";
+    newButton.setAttribute("data-confirm", "false");
+
     newLi.appendChild(newButton);
+
     if (transaction.type === "receita") {
       newLi.classList.add("receita");
     } else {
       newLi.classList.add("despesa");
     }
+
     transactionList.appendChild(newLi);
   });
 }
 
 function removeTransaction(event) {
   if (event.target.tagName === "BUTTON") {
-    const searchIndex = transactions.findIndex(
-      (transaction) =>
-        transaction.id === +event.target.parentElement.dataset.id,
-    );
-    if (searchIndex === -1) {
+    if (event.target.dataset.confirm === "true") {
+      const searchIndex = transactions.findIndex(
+        (transaction) =>
+          transaction.id === +event.target.parentElement.dataset.id,
+      );
+
+      if (searchIndex === -1) {
+        return;
+      }
+
+      transactions.splice(searchIndex, 1);
+
+      saveTransactions();
+      calculateFinancialSummary();
+      updateFinancialSummary();
+      updateChart();
+      updateCategoryChart();
+      updateRevenueCategoryChart();
+      filterTransactions();
+
       return;
     }
-    transactions.splice(searchIndex, 1);
 
-    saveTransactions();
-    calculateFinancialSummary();
-    updateFinancialSummary();
-    updateChart();
-    updateCategoryChart();
-    filterTransactions();
+    if (event.target.dataset.confirm === "false") {
+      event.target.textContent = "Confirmar?";
+      event.target.dataset.confirm = "true";
+    }
   }
 }
 
 function getTransactions(event) {
   event.preventDefault();
+
   const description = inputDescription.value.trim();
   const value = +inputValue.value;
   const type = transactionTypeSelect.value;
@@ -201,18 +258,23 @@ function getTransactions(event) {
       category,
       date,
     };
+
     transactions.push(transaction);
+
     inputDescription.value = "";
     inputValue.value = "";
     inputDate.value = "";
+
     filterTransactions();
     calculateFinancialSummary();
     updateFinancialSummary();
     updateChart();
     updateCategoryChart();
+    updateRevenueCategoryChart();
     saveTransactions();
   }
 }
+
 function getChartData(revenue, expense) {
   const financialData = {
     revenue,
@@ -239,6 +301,7 @@ function createChart() {
         {
           label: "Resumo financeiro",
           data: values,
+
           backgroundColor: [
             "rgba(46, 204, 113, 1.0)",
             "rgba(231, 76, 60, 1.0)",
@@ -248,6 +311,7 @@ function createChart() {
     },
   });
 }
+
 function createCategoryChart() {
   const categoryData = getExpensesByCategory();
 
@@ -266,11 +330,65 @@ function createCategoryChart() {
     },
   });
 }
+
+function createRevenueCategoryChart() {
+  const categoryData = getRevenuesByCategory();
+
+  revenueCategoryChart = new Chart(revenuesCategoryChart, {
+    type: "doughnut",
+
+    data: {
+      labels: categoryData.labels,
+
+      datasets: [
+        {
+          label: "Receitas por categoria",
+          data: categoryData.values,
+        },
+      ],
+    },
+  });
+}
+
+function updateRevenueCategoryChart() {
+  const categoryData = getRevenuesByCategory();
+
+  revenueCategoryChart.data.labels = categoryData.labels;
+  revenueCategoryChart.data.datasets[0].data = categoryData.values;
+
+  revenueCategoryChart.update();
+}
+
+function getRevenuesByCategory() {
+  const revenues = transactions.filter((transaction) => {
+    return transaction.type === "receita";
+  });
+
+  const revenuesByCategory = revenues.reduce((accumulator, transaction) => {
+    if (!accumulator[transaction.category]) {
+      accumulator[transaction.category] = 0;
+    }
+
+    accumulator[transaction.category] += transaction.value;
+
+    return accumulator;
+  }, {});
+
+  const labels = Object.keys(revenuesByCategory);
+  const values = Object.values(revenuesByCategory);
+
+  return {
+    labels,
+    values,
+  };
+}
+
 function updateChart() {
   chart.data.datasets[0].data = [totalRevenue, totalExpense];
 
   chart.update();
 }
+
 function updateCategoryChart() {
   const categoryData = getExpensesByCategory();
 
@@ -279,6 +397,7 @@ function updateCategoryChart() {
 
   categoryChart.update();
 }
+
 function getExpensesByCategory() {
   const expenses = transactions.filter((transaction) => {
     return transaction.type === "despesa";
@@ -293,6 +412,7 @@ function getExpensesByCategory() {
 
     return accumulator;
   }, {});
+
   const labels = Object.keys(expensesByCategory);
   const values = Object.values(expensesByCategory);
 
@@ -309,15 +429,21 @@ filterCategorySelect.addEventListener("change", filterTransactions);
 filterDate.addEventListener("change", filterTransactions);
 btnClearFilters.addEventListener("click", clearFilters);
 form.addEventListener("submit", getTransactions);
+
 loadTransactions();
+
 updateCategories(
   categoriesByType[transactionTypeSelect.value],
   transactionCategorySelect,
 );
+
 updateCategories(allCategories, filterCategorySelect);
+
 filterTransactions();
 calculateFinancialSummary();
 updateFinancialSummary();
 
 createChart();
 createCategoryChart();
+createRevenueCategoryChart();
+updateRevenueCategoryChart();
